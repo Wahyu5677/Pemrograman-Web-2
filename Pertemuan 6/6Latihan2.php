0e149277c394efaf020512ced1c6b6ef3a02f2e7
Wahyu5677 <taqdirwahyu546@gmail.com>
2026-09-25 23:52:35 +0700

    Add files via upload

---
+++ Pertemuan 6/6Latihan2.php
@@ -0,0 +1,22 @@
+<html>
+    <body>
+        <?php
+        //Kode PHP kotak kiri
+        $x=array("one","two","three");
+        foreach($x as $value)
+            {
+                echo $value. "<br/>";
+            }
+
+        echo "<br/>";
+
+        //Kode PHP kotak kanan
+        $b["sayur"] = "wortel";
+        $b["daging"] = "ayam";
+        $b["utama"] = "nasi";
+        $jumlah = sizeof($b);
+        print "Jumlah array b = $jumlah <br>";
+        //variabel $jumlah akan bernilai 4
+        ?>
+    </body>
+</html>
