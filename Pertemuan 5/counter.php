bce400df5ad1734b3966fe126fbed2df488cb318
Wahyu5677 <taqdirwahyu546@gmail.com>
2026-09-25 23:59:04 +0700

    Add files via upload

---
+++ Pertemuan 5/counter.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>Counter</title>
+    </head>
+    <body>
+        <?php
+        $nama_file="counter.dat";
+        if (file_exists($nama_file))
+            {
+                $berkas = fopen($nama_file,"r");
+                $pencacah = (int)trim(fgets($berkas,255));
+                $pencacah++;
+                Fclose ($berkas);
+                }
+                Else
+                $pencacah = 1;
+                //simpan pencacah
+                $berkas = fopen($nama_file,"w");
+                Fputs($berkas, $pencacah);
+                fclose($berkas);
+                
+                //tulis ke halaman web
+                Print("Anda pengunjung ke-$pencacah<br>\n"); 
+            ?>
+                </body>
+                </html>
